816af56ca3ebefabb514e8ac4b80cf19e5c9d5aa
Ojas Maheshwari <ojasmaheshwari2006@gmail.com>
2024-06-28 14:04:32 +0530

    test

--- server/routes/getblogsindescorder.js
+++ server/routes/getblogsindescorder.js
@@ -5,9 +5,11 @@ const Blog = require('../models/Blog');
 router.get('/', async (req, res) => {
 	try {
     const blogs = await Blog.find().sort({ likes: -1 });
+		console.log(blogs);
     res.json(blogs);
   } catch (err) {
     res.status(500).json({ message: err.message });
+		console.log(err);
   }
 });
 
