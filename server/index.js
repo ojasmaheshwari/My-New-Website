f25217adde269753821f174601beb52e0f0b33b3
Ojas Maheshwari <ojasmaheshwari2006@gmail.com>
2024-06-29 08:13:19 +0530

    test

--- server/index.js
+++ server/index.js
@@ -50,7 +50,7 @@ const app = express();
 
 // CORS middleware
 const allowCrossDomain = (req, res, next) => {
-  res.header(`Access-Control-Allow-Origin`, `ojasmaheshwari.github.io`);
+  res.header(`Access-Control-Allow-Origin`, `https://ojasmaheshwari.github.io`);
   res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
   res.header(`Access-Control-Allow-Headers`, `Content-Type`);
   next();
