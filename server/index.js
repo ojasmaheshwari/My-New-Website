043648cf8a9cb4cec4bb48b840f58e96ceb261e9
Ojas Maheshwari <ojasmaheshwari2006@gmail.com>
2024-06-29 08:24:02 +0530

    test

--- server/index.js
+++ server/index.js
@@ -53,6 +53,7 @@ const allowCrossDomain = (req, res, next) => {
   res.header(`Access-Control-Allow-Origin`, `https://ojasmaheshwari.github.io`);
   res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
   res.header(`Access-Control-Allow-Headers`, `Content-Type`);
+	res.header(`Access-Control-Allow-Credentials`, `true`);
   next();
 };
 
